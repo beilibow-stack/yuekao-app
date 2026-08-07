7f4f5b235cff2f4f4e5826c18d6c7de0d89f591b
beilibow-stack <beilibow@gmail.com>
2026-08-07 10:56:49 +0800

    添加 next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,8 @@
+/Users/bow/.zprofile:11: no such file or directory: /usr/local/bin/brew
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+import "./.next/types/routes.d.ts";
+import "./.next/types/root-params.d.ts";
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
